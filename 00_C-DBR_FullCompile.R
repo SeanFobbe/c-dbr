4a89e2de60cb16b01be5ece3497f8d2618d63ac7
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-01-10 04:17:36 +0100

    update makefile

--- 00_C-DBR_FullCompile.R
+++ 00_C-DBR_FullCompile.R
@@ -3,7 +3,14 @@
 #groundhog.library(pkg = "rmarkdown",
 #                  date = "2021-02-20")
 
+
+#'# Vorbereitung
+
 library(rmarkdown)
+
+datestamp <- Sys.Date()
+
+
 
 
 
@@ -25,7 +32,7 @@ unlink("temp", recursive = TRUE)
 
 
 #+
-#'### Datensatz 
+#'# Datensatz 
 #' 
 #' Um den **vollständigen Datensatz** zu kompilieren und einen PDF-Bericht zu erstellen, kopieren Sie bitte alle im Source-Archiv bereitgestellten Dateien in einen leeren Ordner und führen mit R diesen Befehl aus:
 
@@ -35,7 +42,7 @@ begin.compreport <- Sys.time()
 rmarkdown::render(input = "01_C-DBR_CorpusCreation.R",
 #                  envir = new.env(),
                   output_file = paste0("C-DBR_",
-                                       Sys.Date(),
+                                       datestamp,
                                        "_CompilationReport.pdf"),
                   output_dir = "output")
 
@@ -45,7 +52,7 @@ print(end.compreport-begin.compreport)
 
 
 
-#'### Codebook
+#'# Codebook
 #' Um das **Codebook** zu kompilieren und einen PDF-Bericht zu erstellen, führen Sie bitte im Anschluss an die Kompilierung des Datensatzes (!) untenstehenden Befehl mit R aus.
 #'
 #' Bei der Prüfung der GPG-Signatur wird ein Fehler auftreten und im Codebook dokumentiert, weil die Daten nicht mit meiner Original-Signatur versehen sind. Dieser Fehler hat jedoch keine Auswirkungen auf die Funktionalität und hindert die Kompilierung nicht.
@@ -54,6 +61,6 @@ print(end.compreport-begin.compreport)
 rmarkdown::render(input = "02_C-DBR_CodebookCreation.R",
                   envir = new.env(),
                   output_file = paste0("C-DBR_",
-                                       Sys.Date(),
+                                       datestamp,
                                        "_Codebook.pdf"),
                   output_dir = "output")
