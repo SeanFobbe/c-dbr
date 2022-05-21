a66b77c41aa28634c2b35aff2551d32b4e866f14
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-05-21 22:19:58 +0200

    readme in externe datei verschieben

--- 01_C-DBR_CorpusCreation.R
+++ 01_C-DBR_CorpusCreation.R
@@ -23,69 +23,12 @@ knitr::opts_chunk$set(echo = TRUE,
                       message = TRUE)
 
 
-#'# Einleitung
-
-#+
-#'## Überblick
-#' Dieses Skript wertet das amtliche Internetangebot \enquote{Gesetze im Internet} (\url{https://www.gesetze-im-internet.de}) der Bundesrepublik Deutschland vollständig aus und kompiliert es in einen reichhaltigen menschen- und maschinenlesbaren Korpus. Es ist die Grundlage des **\datatitle\ (\datashort)**.
-#'
-#' Alle mit diesem Skript erstellten Datensätze werden dauerhaft kostenlos und urheberrechtsfrei auf Zenodo, dem wissenschaftlichen Archiv des CERN, veröffentlicht. Alle Versionen sind mit einem persistenten Digital Object Identifier (DOI) versehen. Die neueste Version des Datensatzes ist immer über den Link der Concept DOI erreichbar: \url{https://doi.org/10.5281/zenodo.3832111}
-
-
-#+
-#'## Endprodukte
-
-#' Primäre Endprodukte des Skripts (im Ordner 'output') sind folgende ZIP-Archive:
-#'
-#' \begin{enumerate}
-#' \item Der volle Datensatz im CSV-Format, unterteilt in Einzelnormen; nur Rechtsakte mit veröffentlichtem Normtext sind erfasst
-#' \item Die Metadaten aller Einzelnormen im CSV-Format (wie 1, nur ohne Normtexte)
-#' \item Der volle Datensatz im CSV-Format, unterteilt in Rechtsakte; nur Rechtsakte mit veröffentlichtem Normtext sind erfasst
-#' \item Die Metadaten aller Rechtsakte im CSV-Format (wie 3, nur ohne Normtexte)
-#' \item Die Metadaten aller auf \enquote{Gesetze im Internet} als XML veröffentlichten Rechtsakte, im CSV-Format, unabhängig davon ob sie Normtext enthalten oder nicht
-#' \item Der volle Datensatz im XML-Format, unterteilt in Rechtsakte; Grundlage für die CSV-Varianten
-#' \item Alle Anlagen zu den XML-Dateien im jeweiligen Original-Format
-#' \item Alle Rechtstexte im TXT-Format, unterteilt in Rechtsakte (deutlich reduzierter Umfang an Metadaten)
-#' \item Alle Rechtstexte im PDF-Format, unterteilt in Rechtsakte (deutlich reduzierter Umfang an Metadaten)
-#' \item Alle Rechtstexte im EPUB-Format, unterteilt in Rechtsakte (deutlich reduzierter Umfang an Metadaten)
-#' \item Alle Analyse-Ergebnisse (Tabellen als CSV, Grafiken als PDF und PNG)
-#' \item Netzwerk-Strukturen (Adjazenzmatrizen, Edgelists, GraphML, und Netzwerk-Diagramme) für alle Rechtsakte (experimentell!)
-#' \end{enumerate}
-#'
-#' Zusätzlich werden für alle ZIP-Archive kryptographische Signaturen (SHA2-256 und SHA3-512) berechnet und in einer CSV-Datei hinterlegt. Die Analyse-Ergebnisse werden zum Ende hin nicht gelöscht, damit sie für die Codebook-Erstellung verwendet werden können. Weiterhin kann optional ein PDF-Bericht erstellt werden (siehe unter "Kompilierung").
-
-
-#'\newpage
-#+
-#'## Kompilierung
-#' Mit der Funktion **render()** von **rmarkdown** können der **vollständige Datensatz** und das **Codebook** kompiliert und die Skripte mitsamt ihrer Rechenergebnisse in ein gut lesbares PDF-Format überführt werden.
-#'
-#' Alle Kommentare sind im roxygen2-Stil gehalten. Die beiden Skripte können daher auch **ohne render()** regulär als R-Skripte ausgeführt werden. Es wird in diesem Fall kein PDF-Bericht erstellt und Diagramme werden nicht abgespeichert.
-#' 
-#' Um den **vollständigen Datensatz** zu kompilieren, sowie Compilation Report und Codebook zu erstellen, kopieren Sie bitte alle im Source-Archiv bereitgestellten Dateien in einen leeren Ordner und führen mit R diesen Befehl aus:
-
-#+ eval = FALSE
-
-source("00_C-DBR_FullCompile.R")
-
-
-#+
-#'## Systemanforderungen
-#' Das Skript in seiner veröffentlichten Form kann nur unter Linux ausgeführt werden, da es Linux-spezifische Optimierungen (z.B. Fork Cluster) und Shell-Kommandos (z.B. OpenSSL) nutzt. Das Skript wurde unter Fedora Linux entwickelt und getestet. Die zur Kompilierung benutzte Version entnehmen Sie bitte dem **sessionInfo()**-Ausdruck am Ende dieses Berichts.
-#'
-#' In der Standard-Einstellung wird das Skript vollautomatisch die maximale Anzahl an Rechenkernen/Threads auf dem System zu nutzen. Wenn die Anzahl Threads (Variable "fullCores") auf 1 gesetzt wird, ist die Parallelisierung deaktiviert.
-#'
-#' Auf der Festplatte sollten 8 GB Speicherplatz vorhanden sein.
-#' 
-#' Um die PDF-Berichte kompilieren zu können benötigen Sie das R package **rmarkdown**, eine vollständige Installation von \LaTeX\ und alle in der Präambel-TEX-Datei angegebenen \LaTeX\ Packages.
-
-
 
 #'\newpage
 
 
 #+ results = "asis", echo = FALSE
-cat(readLines("README.md"),
+cat(readLines("README.md")[-1:2],
     sep = "\n")
 
 
