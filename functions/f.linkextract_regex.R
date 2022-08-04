4ac6b75fdd06e6711dd60e574d51e69b03b5a1e5
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-08-05 00:35:23 +0200

    add specific regex extract fun

---
+++ functions/f.linkextract_regex.R
@@ -0,0 +1,29 @@
+
+#' Extract all links from a html page and return only those indicated by the regular expression.
+
+
+#' @param URL A valid URL
+#' @param regex A valid regular expression.
+
+#' @return A vector of links.
+
+
+f.linkextract_regex <- function(URL,
+                                regex){
+    tryCatch({
+
+        html <- read_html(URL)
+        nodes <- html_nodes(html, "a")
+        links <- html_attr(nodes, 'href')
+        
+        grep(regex,
+             links,
+             ignore.case = TRUE,
+             value = TRUE)
+        
+    },
+    error = function(cond) {
+        return(NA)}
+    )
+}
+
