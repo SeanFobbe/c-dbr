53e3405cfbd0537627c66ac2b765a5a79f20ac4a
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-11-20 23:43:38 +0100

    add html landing pages extraktion

---
+++ functions/f.html_landing_pages.R
@@ -0,0 +1,71 @@
+
+
+
+f.html_landing_pages <- function(url.xml,
+                                 multicore = TRUE,
+                                 cores = parallel::detectCores()){
+
+    if(multicore == TRUE){
+
+        future::plan("multicore",
+                     workers = cores)
+        
+    }else{
+
+        future::plan("sequential")
+
+    }
+
+    
+    ## HTML URLs erstellen
+    url.html <- gsub("/xml.zip",
+                     "/index.html",
+                     url.xml)
+
+
+    ## Landing Pages auswerten
+    names.list <- future.apply::future_lapply(url.html,
+                                              f.linkextract_regex,
+                                              regex = "(.pdf$)|(.epub$)")
+
+
+
+    ## Dateinamen von PDF und EPUB-Dateien in separate Vektoren sortieren
+
+    names.pdf <- lapply(names.list, grep, pattern = "pdf", value = TRUE)
+    names.pdf <- lapply(names.pdf, f.zero.NA)
+    filenames.pdf <- unlist(names.pdf)
+
+
+    names.epub <- lapply(names.list, grep, pattern = "epub", value = TRUE)
+    names.epub <- lapply(names.epub, f.zero.NA)
+    filenames.epub <- unlist(names.epub)
+
+
+
+    ## Test: Gleiche Länge der Dateinamen-Vektoren
+
+
+    if (!length(url.html) == length(filenames.pdf)){
+
+        stop("PDF-Dateinamen sind fehlerhaft.")
+        
+    }
+
+
+    if (!length(url.html) == length(filenames.epub)){
+
+        stop("EPUB-Dateinamen sind fehlerhaft.")
+        
+    }
+
+
+    dt <- data.table(filenames.pdf,
+                     filenames.epub)
+    
+    
+    return(dt)
+    
+
+}
+
