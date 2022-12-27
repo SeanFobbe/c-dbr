57f8fad46be2973a5d5b83007f3f3a13b699e488
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-12-27 08:34:47 +0100

    update multihash fun

--- functions/f.future_multihashes.R
+++ functions/f.future_multihashes.R
@@ -1,3 +1,45 @@
+#' Computation of SHA2 and SHA3 Hashes
+
+#' Computes SHA2-256 and SHA3-512 hashes for a single file. It returns a data frame with the file name, SHA2-256 hashe and SHA3-512 hash.  The function requires the existence of the openssl (RPM) library on the system.
+
+
+#' @param x Path to file.
+
+
+#+
+#'## Required: OpenSSL System Library
+#' The function requires the existence of the openssl (RPM) library on the system.
+
+
+f.multihashes <- function(x){
+    sha2.256 <- system2("openssl",
+                        paste("sha256",
+                              x),
+                        stdout = TRUE)
+    
+    sha2.256 <- gsub("^.*\\= ",
+                     "",
+                     sha2.256)
+    
+    sha3.512 <- system2("openssl",
+                        paste("sha3-512",
+                              x),
+                        stdout = TRUE)
+    
+    sha3.512 <- gsub("^.*\\= ",
+                     "",
+                     sha3.512)
+    
+    hashes <- data.frame(x,
+                         sha2.256,
+                         sha3.512)
+    return(hashes)
+}
+
+
+
+
+
 #' Parallel Computation of SHA2 and SHA3 Hashes
 
 #' This function parallelizes computation of both SHA2-256 and SHA3-512 hashes for an arbitrary number of files. It returns a data frame of file names, SHA2-256 hashes and SHA3-512 hashes. The function requires the existence of the openssl library (RPM) on the system.
@@ -10,7 +52,7 @@
 
 
 
-future_multihashes <- function(x){
+f.future_multihashes <- function(x){
     
     ## Timestamp: Begin
     begin <- Sys.time()
@@ -23,7 +65,7 @@ future_multihashes <- function(x){
     
     ## Compute Hashes
     hashes.list <- future.apply::future_lapply(x,
-                                               multihashes)
+                                               f.multihashes)
 
     ## Coerce List to data.table
     hashes.table <- data.table::rbindlist(hashes.list)
