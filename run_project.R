648dc8728252f7b638bc3c3ccbf1565187a1285b
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-12-30 00:04:29 +0100

    add cleanrun option to config

--- run_project.R
+++ run_project.R
@@ -1,10 +1,42 @@
-# Run full pipeline
+# This script will run the full data pipeline.
+
+
+
+# Timing
+
+begin <- Sys.time()
+
+
+
+
+# Set Directory
 
 dir.out <- "output"
 
 dir.create(dir.out, showWarnings = FALSE)
 
+
+
+
+# Load Config
+
 config <- RcppTOML::parseTOML("config.toml")
+
+
+
+
+# Execute Clean Run (see config)
+
+
+if(config$debug$cleanrun == TRUE){
+
+    source("delete_all_data.R")
+
+    }
+
+
+
+# Run Pipeline
 
 rmarkdown::render("pipeline.Rmd",
                   output_file = file.path(dir.out,
@@ -13,3 +45,10 @@ rmarkdown::render("pipeline.Rmd",
                                                  Sys.Date(),
                                                  "_CompilationReport.pdf")))
 
+
+
+# Report Timing
+
+end <- Sys.time()
+
+print(end-begin)
