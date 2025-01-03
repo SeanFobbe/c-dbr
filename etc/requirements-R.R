c6a5b6a3da6c0cf86c959bc253f184ae761e78f7
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2025-01-03 14:49:52 +0100

    revise R package list and convert to plaintext

--- etc/requirements-R.R
+++ etc/requirements-R.R
@@ -1,35 +1,35 @@
-pkgs <- c("data.table",
-          "future",
-          "future.apply",
-          "targets",
-          "tarchetypes",
-          "qs",
-          "RcppTOML",
-          "mgsub",
-          "readtext",
-          "quanteda",
-          "quanteda.textstats",
-          "quanteda.textplots",
-          "spacyr",
-          "udpipe",
-          "textcat",
-          "igraph",
-          "ggraph",
-          "visNetwork",
-          "ggplot2",
-          "viridis",
-          "RColorBrewer",
-          "scales",
-          "magick",
-          "knitr",
-          "kableExtra",
-          "pdftools",
-          "fs",
-          "httr",
-          "rvest",
-          "renv",
-          "testthat",
-          "zip")
-
-
-install.packages(pkgs)
+data.table
+future
+future.apply
+future.callr
+targets
+tarchetypes
+qs
+bib2df
+RcppTOML
+readtext
+stringi
+quanteda
+quanteda.textstats
+quanteda.textplots
+spacyr
+udpipe
+textcat
+igraph
+ggraph
+visNetwork
+ggplot2
+viridis
+RColorBrewer
+scales
+magick
+knitr
+kableExtra
+pdftools
+fs
+httr
+rvest
+renv
+testthat
+checkmate
+zip
