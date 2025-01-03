9962a08301373be789a59b7bcf2468cac5cd0d79
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2025-01-03 15:00:55 +0100

    update docker delete script

--- docker-delete-all-data.sh
+++ docker-delete-all-data.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-time docker build -t c-dbr:4.4.0 .
-
-time docker-compose run --rm c-dbr Rscript delete-all-data.R
+time docker-compose run --build --rm c-dbr Rscript delete-all-data.R
